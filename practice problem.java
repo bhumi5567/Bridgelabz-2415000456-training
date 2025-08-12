adfb425134d26902d6061c193cac8302c3a915aa
bhumi5567 <bhumika.varshney_cs24@gla.ac.in>
2025-08-12 14:11:12 +0530

    practice problem.java

---
+++ practice problem.java
@@ -0,0 +1,189 @@
+package main;
+/*1
+public class bhu {
+	public static void main(String[] args) {
+		System.out.print("\"Welcome to Bridgelabz!\"");
+	}
+
+}*/
+
+/*2
+ * import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        sc.close();
+
+    }
+}*/
+
+/*3
+ * import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	float a = sc.nextInt();
+    	float b=9/5;
+    	System.out.println((a*b)+32);
+        sc.close();
+    }
+    	
+}*/   	
+/*4
+ * import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        final double pi = 3.14159;
+        System.out.println("Area:"+pi*r*r);
+        sc.close();
+
+    }
+}   */
+
+/*5
+ * import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final double pi=3.14139;
+        int r = sc.nextInt();
+        int h = sc.nextInt();
+        System.out.println("Volume of cylinder:"+pi*r*r*h);
+        sc.close();
+
+    }
+}*/
+
+/*6
+ * import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int r = sc.nextInt();
+        int t = sc.nextInt();
+        System.out.println("Simple Interest:"+(p*r*t)/100);
+        sc.close();
+
+    }
+}*/
+
+/*7
+ * import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final double pi=3.14139;
+        int l = sc.nextInt();
+        int w = sc.nextInt();
+        System.out.println("Perimeter:"+2*(l+w));
+        sc.close();
+
+    }
+}*/
+
+/*8
+ * import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final double pi=3.14139;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        double result = Math.pow(a, b);
+        System.out.println(result);
+        sc.close();
+
+    }
+}*/
+
+/*9
+ * import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final double pi=3.14139;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("Average:"+(a+b+c)/3);
+        sc.close();
+
+    }
+}
+*/
+
+
+import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	float a = sc.nextInt();
+    	double m=a*0.162137;
+    	
+    	
+        
+        System.out.printf("Formatted value: %.4f%n", m);
+        // Output: Formatted value: 123.4568
+        sc.close();
+    }
+    	
+}
+
+
+/*import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter no. sides:");
+        int a = sc.nextInt();
+        System.out.println("enter length:");
+        int b = sc.nextInt();
+        System.out.println("Perimeter:"+a*b);
+        sc.close();																																																																		
+
+    }
+}*/
+
+/*import java.util.Scanner;
+
+public class bhu{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s=0;
+        for(int i=1;i<=n;i++) {
+        	s+=i;
+        }
+        System.out.print(s);
+    }
+}*/
+
+
+
+
+     
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    
