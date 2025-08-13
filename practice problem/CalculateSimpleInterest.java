82ab197ea5a8eb8de583dc3837849bd33d349a0f
bhumi5567 <bhumika.varshney_cs24@gla.ac.in>
2025-08-13 13:22:54 +0530

    Update CalculateSimpleInterest.java

--- practice problem/CalculateSimpleInterest.java
+++ practice problem/CalculateSimpleInterest.java
@@ -1,5 +1,14 @@
 package main;
-
+import java.util.Scanner;
 public class CalculateSimpleInterest {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int r = sc.nextInt();
+        int t = sc.nextInt();
+        System.out.println("Simple Interest:"+(p*r*t)/100);
+        sc.close();
+
+    }
 
 }
