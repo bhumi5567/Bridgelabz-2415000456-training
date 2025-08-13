5ffa4323c102cd78cc24ebdf2a4701a240908ec0
bhumi5567 <bhumika.varshney_cs24@gla.ac.in>
2025-08-13 13:21:58 +0530

    Update CalculateAverageofThreeNumbers.java

--- practice problem/CalculateAverageofThreeNumbers.java
+++ practice problem/CalculateAverageofThreeNumbers.java
@@ -1,5 +1,15 @@
 package main;
-
+import java.util.*;
 public class CalculateAverageofThreeNumbers {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final double pi=3.14139;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("Average:"+(a+b+c)/3);
+        sc.close();
+
+    }
 
 }
