7478531e9a21d5c75ed6dfc2bf58324784819f24
bhumi5567 <bhumika.varshney_cs24@gla.ac.in>
2025-08-13 13:20:55 +0530

    Update AreaofaCircle.java

--- practice problem/AreaofaCircle.java
+++ practice problem/AreaofaCircle.java
@@ -1,5 +1,13 @@
 package main;
-
+import java.util.*;
 public class AreaofaCircle {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        final double pi = 3.14159;
+        System.out.println("Area:"+pi*r*r);
+        sc.close();
+
+    }
 
 }
