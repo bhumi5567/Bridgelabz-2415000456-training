064c9841093f1b883df3e5f2ac437bb45e94abd7
bhumi5567 <bhumika.varshney_cs24@gla.ac.in>
2025-08-13 13:23:50 +0530

    Update PerimeterofaRectangle.java

--- practice problem/PerimeterofaRectangle.java
+++ practice problem/PerimeterofaRectangle.java
@@ -1,5 +1,14 @@
 package main;
-
+import java.util.Scanner;
 public class PerimeterofaRectangle {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final double pi=3.14139;
+        int l = sc.nextInt();
+        int w = sc.nextInt();
+        System.out.println("Perimeter:"+2*(l+w));
+        sc.close();
+
+    }
 
 }
