1ecafcd0556be4b0141b799559811ece60854e6e
bhumi5567 <bhumika.varshney_cs24@gla.ac.in>
2025-08-13 13:24:31 +0530

    Update PowerCalculation.java

--- practice problem/PowerCalculation.java
+++ practice problem/PowerCalculation.java
@@ -1,5 +1,15 @@
 package main;
-
+import java.util.Scanner;
 public class PowerCalculation {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final double pi=3.14139;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        double result = Math.pow(a, b);
+        System.out.println(result);
+        sc.close();
+
+    }
 
 }
