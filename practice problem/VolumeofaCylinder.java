6d2a42d26f6b9e77f849e6c1904f2a30cd24ddaa
bhumi5567 <bhumika.varshney_cs24@gla.ac.in>
2025-08-13 13:25:14 +0530

    Update VolumeofaCylinder.java

--- practice problem/VolumeofaCylinder.java
+++ practice problem/VolumeofaCylinder.java
@@ -1,5 +1,14 @@
 package main;
-
+import java.util.Scanner;
 public class VolumeofaCylinder {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final double pi=3.14139;
+        int r = sc.nextInt();
+        int h = sc.nextInt();
+        System.out.println("Volume of cylinder:"+pi*r*r*h);
+        sc.close();
+
+    }
 
 }
